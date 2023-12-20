2fdebac0e9fc9876d3d1ba52cf8047dffc765c2b
rlaehrla <rlaehrla@naver.com>
2023-12-20 10:34:37 +0900

    12-20일 오전10:34분

---
+++ 3.스프링/spring_study/exam07/src/main/java/models/member/MemberNotFoundException.java
@@ -0,0 +1,12 @@
+package models.member;
+
+import commons.CommonException;
+import org.springframework.http.HttpStatus;
+
+public class MemberNotFoundException extends CommonException {
+
+    public MemberNotFoundException() {
+        super("등록되지 않은 회원입니다.", HttpStatus.NOT_FOUND); // 404
+    }
+
+}
