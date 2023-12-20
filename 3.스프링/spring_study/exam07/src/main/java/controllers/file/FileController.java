9dbb2ae9b98300df9f565b5b0b072c4981cd231a
rlaehrla <rlaehrla@naver.com>
2023-12-20 11:38:28 +0900

    12-20일 오전10:34분

--- 3.스프링/spring_study/exam07/src/main/java/controllers/file/FileController.java
+++ 3.스프링/spring_study/exam07/src/main/java/controllers/file/FileController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/file")
@@ -16,8 +21,17 @@ public class FileController {
     }
 
     @PostMapping("/upload")
-    public String uploadPs() {
+    public String uploadPs(@RequestParam("file") MultipartFile[] files) {
+        for (MultipartFile file : files) {
 
-        return "file/upload";
-    }
+            File uploadPath = new File("C:/uploads/" + file.getOriginalFilename());
+
+            try {
+                file.transferTo(uploadPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+            return "file/upload";
+        }
 }
