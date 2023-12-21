650cb8bad9e86269f156b75cef41d82ae62e97dd
rlaehrla <rlaehrla@naver.com>
2023-12-21 10:51:53 +0900

    스프링부트 12/21목 오전10:51

---
+++ 5.스프링부트/exam01/src/main/java/org/choongang/entities/Member.java
@@ -0,0 +1,20 @@
+package org.choongang.entities;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Member {
+    // USER_NO -> userNo
+
+    @Id // Primary Key : 기본키
+    private Long userNo;
+    private String userId;
+    private String userPw;
+    private String userNm;
+    private String email;
+    private LocalDateTime regDt;
+    private LocalDateTime modDt;
+}
---
+++ 5.스프링부트/exam01/src/main/java/org/choongang/repositories/MemberRepository.java
@@ -0,0 +1,8 @@
+package org.choongang.repositories;
+
+import org.choongang.entities.Member;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+
+}
