d7275127aeba0c773798879447be23bae7d5f9b3
rlaehrla <rlaehrla@naver.com>
2023-12-22 14:57:32 +0900

    스프링부트 exam02 12/22 오후2:57

--- 5.스프링부트/exam01/src/main/java/org/choongang/restcontrollers/ApiMemberController.java
+++ 5.스프링부트/exam01/src/main/java/org/choongang/restcontrollers/ApiMemberController.java
@@ -1,17 +1,54 @@
 package org.choongang.restcontrollers;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.choongang.commons.BadRequestException;
+import org.choongang.commons.JSONData;
 import org.choongang.entities.Member;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/member")
 public class ApiMemberController {
+
+    @PostMapping
+    public ResponseEntity<JSONData> join(@Valid @RequestBody RequestJoin form, Errors errors) {
+        if (errors.hasErrors()) {
+            List<String> messages = errors.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            log.info("에러 : {}", messages.toString());
+
+            String message = messages.stream().collect(Collectors.joining(","));
+
+            throw new BadRequestException(message);
+        }
+
+        // 응답 코드 - 201, Body - 없음
+
+        HttpStatus status = HttpStatus.CREATED;
+        JSONData<Object> data = new JSONData<>();
+        data.setStatus(status);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(data);
+        /*
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("CUSTOM_HEADER", "value1")
+                .build();
+
+         */
+    }
 
     @GetMapping
     public Member info() {
@@ -25,21 +62,26 @@ public class ApiMemberController {
                 .modDt(LocalDateTime.now())
                 .build();
 
+        //JSONData<Member> data = new JSONData<>();
+        //data.setData(member);
+
+        //return new JSONData<>(member);
         return member;
     }
 
     @GetMapping("/list")
     public List<Member> list() {
-        List<Member> members = IntStream.rangeClosed(1, 10).mapToObj(i -> Member.builder()
-                .userNo(Long.valueOf(i))
-                .userId("user" + i)
-                .userPw("12345678")
-                .userNm("사용자" + i)
-                .email("user" + i + "@test.org")
-                .regDt(LocalDateTime.now())
-                .modDt(LocalDateTime.now())
-                .build()
-            ).toList();
+        List<Member> members = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Member.builder()
+                        .userNo(Long.valueOf(i))
+                        .userId("user" + i)
+                        .userPw("12345678")
+                        .userNm("사용자" + i)
+                        .email("user" + i + "@test.org")
+                        .regDt(LocalDateTime.now())
+                        .modDt(LocalDateTime.now())
+                        .build()
+                ).toList();
         return members;
     }
 
@@ -51,6 +93,15 @@ public class ApiMemberController {
 
     @GetMapping("/process")
     public void process() {
-        System.out.println("처리......");
+        System.out.println("처리....");
     }
+
+    /*
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity errorHandler(Exception e) {
+
+
+       // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }*/
 }
---
+++ 5.스프링부트/exam01/src/test/java/org/choongang/tests/ApiTest.java
@@ -0,0 +1,96 @@
+package org.choongang.tests;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.choongang.entities.Member;
+import org.choongang.restcontrollers.RequestJoin;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("회원 가입 성공시 응답 코드 201")
+    void joinTest() throws Exception {
+        // MediaType.APPLICATION_JSON == application/json
+
+        RequestJoin form = new RequestJoin();
+        form.setUserId("user01");
+        form.setUserPw("12345678");
+        form.setConfirmPw("12345678");
+        form.setUserNm("사용자01");
+        form.setEmail("user01@test.org");
+        form.setRegDt(LocalDateTime.now());
+
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+
+        String body = om.writeValueAsString(form); // 자바 객체 -> JSON 문자열 변환
+
+        System.out.println(body);
+
+        mockMvc.perform(
+                        post("/api/member")
+                                //.header("Content-Type", "application/json")
+                                .header("Content-Type", MediaType.APPLICATION_JSON)
+                                .characterEncoding("UTF-8")
+                                .content(body)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated()); // 201
+    }
+
+    @Test
+    @DisplayName("JSON 문자열 -> Member 객체로 변환")
+    void infoTest() throws Exception {
+        String body = mockMvc.perform(get("/api/member"))
+                .andDo(print())
+                .andReturn().getResponse()
+                .getContentAsString(Charset.forName("UTF-8"));
+
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+
+        Member member = om.readValue(body, Member.class);
+        System.out.println(member);
+
+    }
+
+
+    @Test
+    void listTest() throws Exception {
+        String body = mockMvc.perform(get("/api/member/list"))
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(Charset.forName("UTF-8"));
+
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+
+        List<Member> members = om.readValue(body, new TypeReference<>() {
+        });
+        members.forEach(System.out::println);
+    }
+}
