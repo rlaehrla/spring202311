c22f3956695100d2618d06e91295f013261387aa
rlaehrla <rlaehrla@naver.com>
2023-12-26 11:35:47 +0900

    스프링부트 exam02 12/26 오전11:35

--- 5.스프링부트/exam02/src/main/java/org/choongang/entities/Member.java
+++ 5.스프링부트/exam02/src/main/java/org/choongang/entities/Member.java
@@ -3,27 +3,44 @@ package org.choongang.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.choongang.commons.MemberType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.repository.config.AuditingBeanDefinitionParser;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Data
 @Table(name="USERS", indexes = @Index(name = "idx_member_createdAt", columnList = "createdAt DESC"))
+@EntityListeners(AuditingBeanDefinitionParser.class)
 public class Member {
     @Id @GeneratedValue
     private Long seq;
     private String email;
     private String name;
     private String password; // varchar2
-    @Lob
+    //@Lob
+    @Transient
     private String introduction; // CLOB
 
-    @CreationTimestamp // INSERT SQL 실행시
+    @Enumerated(EnumType.STRING)
+    private MemberType type;
+
+    //@CreationTimestamp // INSERT SQL 실행시
+    @CreatedDate
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp // UPDATE SQL 실행시
+    //@UpdateTimestamp // UPDATE SQL 실행시
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    //@Temporal(TemporalType.DATE) // 날짜
+    //@Temporal(TemporalType.TIME) // 시간
+    //@Temporal(TemporalType.TIMESTAMP) // 날짜 + 시간
+    //public Date dt;
 }
 
